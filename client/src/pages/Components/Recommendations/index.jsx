3703f1da47c5407aa315b7bb1af3faa3eac0f308
M.Dinesh <dinesh372003@gmail.com>
2022-01-18 19:05:31 +0530

    Fliping recommendation Card

--- client/src/pages/Components/Recommendations/index.jsx
+++ client/src/pages/Components/Recommendations/index.jsx
@@ -1,45 +1,87 @@
 import React from "react";
 
 import Styles from "./Style.module.css";
+import img from "../../../assets/images/logo192.png";
 
 function index() {
 	return (
 		<div className={Styles.container}>
+
 			<div className={Styles.arrows1}>
 				<i id={Styles.arrow1} className="fas fa-2x fa-chevron-left" />
 			</div>
+
 			<div className={Styles.cardscroll}>
+
 				<div className={Styles.card}>
-					<img src="images/logo192.png" className={Styles.profile} alt="img" />
-					<div className={Styles.Name}>
-						ABCD
-					</div>
-					<div className={Styles.description}>
-						I am ABCD
+					<div className={Styles.cardinner}>
+
+						<div className={Styles.cardfront}>
+							<img src={img} className={Styles.profile} alt="img" />
+							<div className={Styles.Name}>
+								ABCD
+							</div>
+							<div className={Styles.description}>
+								I am ABCD
+							</div>
+						</div>
+
+						<div className={Styles.cardback}>
+							<div className={Styles.wordwrap}>
+								123456789101112131415161718192021222324252627282930
+							</div>
+						</div>
+
 					</div>
 				</div>
+
 				<div className={Styles.card}>
-					<img src="images/logo192.png" className={Styles.profile} alt="img" />
-					<div className={Styles.Name}>
-						EFGH
-					</div>
-					<div className={Styles.description}>
-						Hi and hello
+					<div className={Styles.cardinner}>
+
+						<div className={Styles.cardfront}>
+							<img src={img} className={Styles.profile} alt="img" />
+							<div className={Styles.Name}>
+								EFGH
+							</div>
+							<div className={Styles.description}>
+								Hi and hello
+							</div>
+						</div>
+
+						<div className={Styles.cardback}>
+							<div className={Styles.wordwrap}>
+								ABDCEFGHIJKLMNOPQRSTUVWXYZ
+							</div>
+						</div>
+
 					</div>
 				</div>
+
 				<div className={Styles.card}>
-					<img src="images/logo192.png" className={Styles.profile} alt="img" />
-					<div className={Styles.Name}>
-						IJKL
-					</div>
-					<div className={Styles.description}>
-						Hi and hello
+					<div className={Styles.cardinner}>
+						<div className={Styles.cardfront}>
+							<img src={img} className={Styles.profile} alt="img" />
+							<div className={Styles.Name}>
+								IJKL
+							</div>
+							<div className={Styles.description}>
+								Hi and hello
+							</div>
+						</div>
+						<div className={Styles.cardback}>
+							<div className={Styles.wordwrap}>
+								Hello this is helloabcd 123
+							</div>
+						</div>
 					</div>
 				</div>
+
 			</div>
+
 			<div className={Styles.arrows2}>
 				<i id={Styles.arrow2} className="fas fa-2x fa-chevron-right" />
 			</div>
+
 		</div>
 	);
 }
